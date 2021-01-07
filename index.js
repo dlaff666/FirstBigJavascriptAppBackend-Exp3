a2643b7051a2b2820452de10911bcc0c7ba346b6
DLaff666 <dlaff666@gmail.com>
2021-01-07 14:48:14 +0000

    Changed indentation in index.js
    Commented HTML in index.html

--- index.js
+++ index.js
@@ -3,42 +3,42 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/eventhandlers.js', (req, res) => {
-  res.sendFile(__dirname + '/eventhandlers.js');
+    res.sendFile(__dirname + '/eventhandlers.js');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected with socket id ' + socket.id);
-  //console.log(socket.id);
-  //console.log(socket.client.conn.server);
-  io.emit('connect-count', socket.client.conn.server.clientsCount);
-  io.emit('connect-user', {"socketId": socket.id});
-
-  // Chat Message Listener
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
-  });
-
-  // User Update Listener
-  socket.on('user-update', (jsonData) => {
-    io.emit('user-update', jsonData);
-  });
-
-  // User Typing Listener
-  socket.on('user-typing', (jsonData) => {
-    io.emit('user-typing', jsonData);
-  });
-
-  // Disconnect Event Listener 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log('a user connected with socket id ' + socket.id);
+    //console.log(socket.id);
+    //console.log(socket.client.conn.server);
     io.emit('connect-count', socket.client.conn.server.clientsCount);
-    io.emit('disconnect-user', {"socketId": socket.id});
-  });
+    io.emit('connect-user', {"socketId": socket.id});
+
+    // Chat Message Listener
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
+
+    // User Update Listener
+    socket.on('user-update', (jsonData) => {
+        io.emit('user-update', jsonData);
+    });
+
+    // User Typing Listener
+    socket.on('user-typing', (jsonData) => {
+        io.emit('user-typing', jsonData);
+    });
+
+    // Disconnect Event Listener 
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('connect-count', socket.client.conn.server.clientsCount);
+        io.emit('disconnect-user', {"socketId": socket.id});
+    });
 
 });
 
