f5d19777fac2d1a23b60cbea67e177805518228e
DLaff666 <dlaff666@gmail.com>
2021-01-07 15:12:42 +0000

    Commented index.js

--- index.js
+++ index.js
@@ -2,38 +2,42 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+// Route for index.html
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// Route for eventhandlers.js
 app.get('/eventhandlers.js', (req, res) => {
     res.sendFile(__dirname + '/eventhandlers.js');
 });
 
 io.on('connection', (socket) => {
+
+    // New connection listener
     console.log('a user connected with socket id ' + socket.id);
-    //console.log(socket.id);
-    //console.log(socket.client.conn.server);
+    /*console.log(socket.id);
+    console.log(socket.client.conn.server);*/
     io.emit('connect-count', socket.client.conn.server.clientsCount);
     io.emit('connect-user', {"socketId": socket.id});
 
-    // Chat Message Listener
+    // Chat message listener
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
         console.log('message: ' + msg);
     });
 
-    // User Update Listener
+    // User update listener
     socket.on('user-update', (jsonData) => {
         io.emit('user-update', jsonData);
     });
 
-    // User Typing Listener
+    // User typing listener
     socket.on('user-typing', (jsonData) => {
         io.emit('user-typing', jsonData);
     });
 
-    // Disconnect Event Listener 
+    // Disconnect event listener 
     socket.on('disconnect', () => {
         console.log('user disconnected');
         io.emit('connect-count', socket.client.conn.server.clientsCount);
@@ -42,6 +46,7 @@ io.on('connection', (socket) => {
 
 });
 
+// Listener
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
